943b5ead0edcfa667029796b8c4b7086c7ce2b19
Samuelposada331 <samuelposada7@gmail.com>
2020-11-24 23:10:41 -0500

    js

--- js/aldd.js
+++ js/aldd.js
@@ -1,6 +1,18 @@
-alert("este es mi proyecto")
+
 $(function(){
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
   });
 
+document.getElementById("hombre").onmouseover = function (){
+    var imagenes = document.getElementsByClassName("card-img-top").style.width="100%";
+    var imagenes = document.getElementsByClassName("card-img-top").style.height="100%";
+    
+}
+
+document.getElementById("hombre").onmouseout = function (){
+    var portadahoteles = document.getElementsByClassName("card-img-top").style.width="500px";
+    var portadahoteles = document.getElementsByClassName("card-img-top").style.height="100%";
+
+    
+}
