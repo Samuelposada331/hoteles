1d77411229cb9f2a725e6d4a3bc4175c224898ba
Samuelposada331 <samuelposada7@gmail.com>
2020-11-25 00:57:49 -0500

    js

--- js/aldd.js
+++ js/aldd.js
@@ -3,18 +3,9 @@ $(function(){
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
   });
-
-document.getElementById("catalogo").onmouseover = function (){
-    var imagenes = document.getElementsByClassName("card-img-top").style.width="100%";
-    var imagenes = document.getElementsByClassName("card-img-top").style.height="100%";
-}
-
-document.getElementById("catalogo").onmouseout = function (){
-    var portadahoteles = document.getElementsByClassName("card-img-top").style.width="500px";
-    var portadahoteles = document.getElementsByClassName("card-img-top").style.height="100%";    
-}
-
 color = "#000000"
 document.getElementById("modonocturno").onclick = function(){
   document.getElementsByTagName("body")[0].style.backgroundColor="gray"
+  document.getElementClassName("btn").style.backgroundColor="btn-outline-success";
+
 }
